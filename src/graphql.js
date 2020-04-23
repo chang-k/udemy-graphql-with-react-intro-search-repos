15cfd75e16f9d70bef70f6ead31487509f0ea443
小曽根健人-私用 <>
2020-04-24 00:46:54 +0900

    setup-pagenation

--- src/graphql.js
+++ src/graphql.js
@@ -1,10 +1,42 @@
 import gql from "graphql-tag"
 
-export const ME = gql`
-  query me {
-    user(login: "iteachonudemy") {
-      name
-      avatarUrl
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories(
+    $after: String,
+    $before: String,
+    $first: Int,
+    $last: Int,
+    $query: String!
+  ) {
+    search(
+      after: $after,
+      before: $before,
+      first: $first,
+      last: $last,
+      query: $query,
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
     }
   }
 `
